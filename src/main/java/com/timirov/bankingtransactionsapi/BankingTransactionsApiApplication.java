4432ec0a9fc29e4cd0c34066d05b245e257d0f61
IlyaTimirov <ilya.ekb.99@mail.ru>
2024-02-29 15:12:26 +0600

    Create project

---
+++ src/main/java/com/timirov/bankingtransactionsapi/BankingTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.timirov.bankingtransactionsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionsApiApplication.class, args);
+	}
+
+}
