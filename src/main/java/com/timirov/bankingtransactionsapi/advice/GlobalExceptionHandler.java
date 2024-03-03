05f5477b2fc4c82b66b8f72d6443f8a633cf6d28
IlyaTimirov <ilya.ekb.99@mail.ru>
2024-03-03 13:15:34 +0600

    treatment Valid Exception

--- src/main/java/com/timirov/bankingtransactionsapi/advice/GlobalExceptionHandler.java
+++ src/main/java/com/timirov/bankingtransactionsapi/advice/GlobalExceptionHandler.java
@@ -12,12 +12,28 @@ import com.timirov.bankingtransactionsapi.exception.phone.PhoneNotUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> resolveHandle(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error ->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName ,message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
--- src/main/java/com/timirov/bankingtransactionsapi/controller/UserController.java
+++ src/main/java/com/timirov/bankingtransactionsapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.timirov.bankingtransactionsapi.controller;
 
 import com.timirov.bankingtransactionsapi.dto.CreateUserDto;
 import com.timirov.bankingtransactionsapi.service.CreateUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ public class UserController {
     private final CreateUserService createUserService;
 
     @PostMapping("/createUser")
-    public ResponseEntity<?> createNewUser(@RequestBody CreateUserDto createUserDto){
+    public ResponseEntity<?> createNewUser(@RequestBody @Valid CreateUserDto createUserDto){
         createUserService.createNewUser(createUserDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
